3126c537e05f327661cbf4fa0aa4985fbe536379
barendlinders <barend.linders@npo.nl>
2019-12-17 08:06:26 +0100

    now with test day 6 pt2

--- day6/day6_test.go
+++ day6/day6_test.go
@@ -19,3 +19,19 @@ func TestPart1(t *testing.T) {
 		assert.Equal(t, tc.expected, part1(tc.in))
 	}
 }
+
+func TestPart2(t *testing.T) {
+	tt := []struct {
+		in       []string
+		expected int
+	}{
+		{in: []string{"COM)A", "A)YOU", "A)SAN"}, expected: 0},
+		{in: []string{"COM)A", "A)B", "B)YOU", "A)C", "C)SAN"}, expected: 2},
+		{in: []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H",
+			"D)I", "E)J", "J)K", "K)L", "I)SAN", "K)YOU"}, expected: 4},
+	}
+
+	for _, tc := range tt {
+		assert.Equal(t, tc.expected, part2(tc.in))
+	}
+}
